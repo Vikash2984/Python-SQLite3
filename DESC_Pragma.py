02d63575e53a0f20dd1470ee88e2a0a523b84f70
Vikash Kumar Pandey <112485773+Vikash2984@users.noreply.github.com>
2024-03-05 20:18:11 +0530

    Create DESC_Pragma.py

---
+++ DESC_Pragma.py
@@ -0,0 +1,31 @@
+# importing the required module
+import sqlite3
+
+#use the database if it pre-exists or create it otherwise
+try:
+    connection = sqlite3.connect("myemp.db")
+    print("Connected with the database successfully .....")
+except:
+    print("Database creation ERROR !!!")
+
+# create employee table as employee (emp_id,emp_name, emp_city, emp_salary)
+sqlStr = '''
+        CREATE TABLE employee(
+        emp_id INTEGER PRIMARY KEY,
+        emp_name TEXT,
+        emp_city TEXT,
+        emp_salary REAL
+        );
+        '''
+try:
+    connection.execute(sqlStr)
+    print("Database table has been created successfully....")
+except:
+    print("Error occurred while creating the datbase ")
+
+
+
+print(connection.execute('pragma table_info(employee)'))
+print(list(connection.execute('pragma table_info(employee)'))) # return list of tuples
+for each_row in list (connection.execute('pragma table_info(employee)')):
+    print(each_row)
